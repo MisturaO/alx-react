454865db5e7751cbb82e29bfe70a10c229ed8e0f
MisturaO <misturaojulari91@gmail.com>
2023-10-24 23:00:28 +0100

    Basic setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,8 +1,7 @@
 import $ from "jquery";
 
 /*imports jquery and add three different paragraphs to the page body when the DOM is ready*/
-$( function() {
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<p>Copyright - Holberton School</p>')
-});
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
