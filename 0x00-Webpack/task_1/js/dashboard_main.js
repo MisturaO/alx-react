446357e1f3708fad93717d4b19337bd828d5d162
MisturaO <misturaojulari91@gmail.com>
2023-10-25 23:37:36 +0100

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -5,14 +5,14 @@ Bind the debounce function in Lodash to the click event on the button:
 */
 
 
-import $ from "jquery";
+import $ from 'jquery';
 import _ from 'lodash';
 
-$('body').append("<p>Holberton Dashboard</p>");
-$('body').append("<p>Dashboard data for the students</p>");
-$('body').append("<button>Click here to get started</button>");
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
-$('body').append("<p>Copyright - Holberton School</p>");
+$('body').append('<p>Copyright - Holberton School</p>');
 
 let count = 0;
 
