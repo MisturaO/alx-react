e7b2762bd175a9a715a36a0b6dc1332f16775140
MisturaO <misturaojulari91@gmail.com>
2023-10-29 11:53:42 +0100

    Dev servers, modules, and tree shaking

---
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,25 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import "./body.css";
+
+
+$('body').append('<img id="logo" />');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+
+function updateCounter(){
+    count++;
+    $('p #count').text(`${count} clicks on the button`);
+};
+
+
+//Sets debounce delay in milliseconds.Prevents accidental clicks on button and delays trigger
+const debounceDelay = 1000;
+
+/* Binds the debounce function to the click event */
+$('button').on('click', _.debounce(updateCounter, debounceDelay));
