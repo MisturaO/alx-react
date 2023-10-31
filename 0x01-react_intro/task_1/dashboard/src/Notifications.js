87c67f3cfbdc3760d6454f53d90f9e43218c2522
MisturaO <misturaojulari91@gmail.com>
2023-10-31 10:29:22 +0100

    Add form to the APP

--- 0x01-react_intro/task_1/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -3,6 +3,7 @@ import './Notifications.css'
 
 
 export default function Notifications(){
+    //Returns html element
     return (
         <div className='Notifications'>
             <p>Here is the list of notifications</p>
