f8eb0741df88f299125889116ca1ba1bf3cabbc1
MisturaO <misturaojulari91@gmail.com>
2023-11-03 02:24:47 +0100

    Create a new project using Webpack without using create-react-app

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, '/src/index.js'), // here's absolute path to the entry point file
+      },
+      output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist') // I used the relative path (../) because the dist directory is one dir up from here.
+      },
+      mode: 'development',
+      plugins: [
+        new webpack.HotModuleReplacementPlugin(), // Enable HMR(enables hot reload)
+      ],
+      devServer: {
+        static: path.resolve(__dirname, '../dist'), // Specify the directory to serve.
+        hot: true, // Enable HMR in the dev server(enables hot module reload in dev server)
+        open: true,
+      },
+      module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [
+                'file-loader',
+                {
+                    loader: 'image-webpack-loader',
+                    options: {
+                      mozjpeg: {
+                        progressive: true,
+                        quality: 65,
+                      },
+                      // Add more image optimization options as needed
+                    },
+                },
+            ],
+          },
+        ],
+      },
+      devtool: 'inline-source-map', // Enable inline source maps
+};
