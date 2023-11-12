bc9889481eea344c51a35c9a6dfad3be6a857462
MisturaO <misturaojulari91@gmail.com>
2023-11-12 20:23:51 +0100

    Shapes, Loops, and Keys

---
+++ 0x02-react_props/task_5/dashboard/src/App/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+import Notifications from '../Notifications/Notifications'; //Moving up one level in the directory structure, and then to the absolute path of the file
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList';
+import {getLatestNotification} from './utils'
+
+
+
+function App({isLoggedIn}) {
+  const listCourses = [//This array of objects will be passed as values to App's(parent) child component (CourseList)
+    {id: 1, name: "ES6", credit: 60},
+    {id: 2, name: "Webpack", credit: 20},
+    {id: 3, name: "React", credit: 40}
+  ];
+
+  const listNotifications = [
+    {id: 1, type: "default", value: "New course available"},
+    {id: 2, type:"urgent", value:"New resume available"},
+    {id: 2, type:"urgent", html: getLatestNotification()}
+  ];
+
+  return (
+    <React.Fragment>
+      <Notifications listNotifications={listNotifications}/>
+      <div className='App'>
+        <Header/>
+      </div>
+      <div className='App-body'>
+        {isLoggedIn ?
+          <Login/> //if isLoggedIn is 'true' render <Login/>, else render <CourseList />
+        : <CourseList listCourses={listCourses}/>} //adds the 'listCourses' prop to this component
+      </div>
+      <div className='App-footer'>
+        <Footer/>
+      </div>    
+    </React.Fragment>
+  );
+};
+
+//Property isLoggedIn should be false by default
+App.propTypes = {
+  isLoggedIn: PropTypes.bool
+}
+App.defaultProps = {
+  isLoggedIn: false
+}
+
+export default App;
