ee419de6a77bc99d7bf4d29a2f49a89427d42d5c
MisturaO <misturaojulari91@gmail.com>
2023-11-16 07:53:59 +0100

    Modify NotificationItem.js as pure component

---
+++ 0x03-React_component/task_5/src/CourseList/CourseListRow.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function CourseListRow({isHeader, textFirstCell, textSecondCell}){//Child component whose props data types are set through the Parent Component's prop
+    // Using ternary operator to conditionally render the elements based on the values of 'isHeader'. Also nested conditions with 'textSecondCell'
+    return (
+    <>
+        <tr>
+            {isHeader ? ( //This check is truthy like: if(isHeader)
+                textSecondCell === null ? (
+                <th colSpan="2">{textFirstCell}</th> //The first test unit in 'CourseListRow.test.js': shows it renders this when 'isHeader' is true
+                ) : (
+                <>
+                    <th>{textFirstCell}</th>
+                    <th>{textSecondCell}</th>
+                </>
+                )) : (
+                    <>
+                        <td>{textFirstCell}</td>
+                        <td>{textSecondCell}</td>
+                    </>)
+                    }
+        </tr>
+        </>
+    );
+}
+
+CourseListRow.propTypes = {
+    isHeader: PropTypes.bool,
+    textFirstCell: PropTypes.string.isRequired,
+    textSecondCell: PropTypes.oneOfType([   //ensures property 'textSecondCell' can accept either a string or number.
+        PropTypes.string,
+        PropTypes.number
+    ])
+}
+
+CourseListRow.defaultProps = {
+    isHeader: false,
+    textSecondCell: null,
+};
+
+export default CourseListRow;
