5c60aef8addf2bb7df7bd5c0e1ed0b1d3a0a7076
MisturaO <misturaojulari91@gmail.com>
2023-11-17 03:57:55 +0100

    Inline styling

--- 0x04-React_inline_styling/task_0/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/src/CourseList/CourseListRow.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, css } from 'aphrodite';
+
 
 function CourseListRow({isHeader, textFirstCell, textSecondCell}){//Child component whose props data types are set through the Parent Component's prop
     // Using ternary operator to conditionally render the elements based on the values of 'isHeader'. Also nested conditions with 'textSecondCell'
+
+    const rowStyle = {
+        backgroundColor: '#f5f5f5ab'
+    }
+
+    const headerStyle = {
+        backgroundColor: '#deb5b545'
+    }
+
     return (
     <>
-        <tr className= {css(styles.rowBackgroundColor)}>
+        <tr style={rowStyle}>
             {isHeader ? ( //This check is truthy like: if(isHeader)
                 textSecondCell === null ? (
-                <th colSpan="2" className={css(styles.headerRow)}>{textFirstCell}</th> //The first test unit in 'CourseListRow.test.js': shows it renders this when 'isHeader' is true
+                <th colSpan="2" style={headerStyle}>{textFirstCell}</th> //The first test unit in 'CourseListRow.test.js': shows it renders this when 'isHeader' is true
                 ) : (
                 <>
-                    <th className={css(styles.headerRow)}>{textFirstCell}</th>
-                    <th className={css(styles.headerRow)}>{textSecondCell}</th>
+                    <th style={headerStyle}>{textFirstCell}</th>
+                    <th style={headerStyle}>{textSecondCell}</th>
                 </>
                 )) : (
                     <>
@@ -39,14 +48,5 @@ CourseListRow.defaultProps = {
     isHeader: false,
     textSecondCell: null,
 };
-
-const styles = StyleSheet.create({
-    rowBackgroundColor: {
-        backgroundColor: '#f5f5f5ab',
-    },
-    headerRow: {
-        backgroundColor: '#deb5b545'
-    }
-});
 
 export default CourseListRow;
