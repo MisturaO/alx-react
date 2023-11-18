fb3927b9727ab598dd42e8788de59f43642bd52b
MisturaO <misturaojulari91@gmail.com>
2023-11-18 02:36:53 +0100

    Responsive design

---
+++ 0x04-React_inline_styling/task_3/src/Notifications/NotificationItem.js
@@ -0,0 +1,67 @@
+import React, {PureComponent} from 'react';
+import propTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+
+class NotificationItem extends PureComponent {
+    constructor(props){
+        super(props)
+    }
+    //TO DEBUG WHAT THE PROPS ARE RETURNING
+    // console.log('type:', type);
+    // console.log('html:', html);
+    // console.log('value:', value);
+    render(){
+        const {type, html, value, style, markAsRead, id} = this.props //class props declaration
+    return (
+        <>
+            {/* 
+            This checks if the props are truthy(if they have values), and if they don't, return null
+            NOTE: We are checking just one <li> element but had to split it to give it separate error checks:
+                <li data-notification-type={type} dangerouslySetInnerHTML={{ __html: html }}>{value}</li>
+            */}
+             {type && value ?  <li className={type === 'default' ? css(styles.defaultList) : css(styles.urgentList)} onClick={() => markAsRead(id)} data-notification-type={type} style={style} >{value}</li> : null}
+            {html ? <li data-testid="Mytest" dangerouslySetInnerHTML={{ __html: html }} onClick={() => markAsRead(id)}></li> : null}
+        </>
+    );
+    }
+};
+
+NotificationItem.propTypes = {
+    id: propTypes.number.isRequired,
+    html: propTypes.shape({ //A html (object of { \_\_html: string })
+        __html: propTypes.string
+    }),
+    type: propTypes.string.isRequired,
+    value: propTypes.string,
+    markAsRead: propTypes.func,
+    id: propTypes.number
+};
+NotificationItem.defaultProps = {
+    id: 0,
+    markAsRead: () => {
+        console.log('empty function');
+    }
+
+}
+
+//Conditionally rendered in the <li> tag
+const styles = StyleSheet.create({
+    defaultList: {
+        color: 'blue'
+    },
+    urgentList: {
+        color: 'red'
+    },
+
+    small: {
+        '@media (max-width: 900px)': {
+            width: '100%',
+            borderBottom: '1px solid black',
+            fontSize: '20px',
+            padding: '10px. 8px'
+        }
+    }
+})
+
+export default NotificationItem;
