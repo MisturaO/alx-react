4bd5adc5d2c2ea9347628704a1ff049dacd20367
MisturaO <misturaojulari91@gmail.com>
2023-12-01 16:11:37 +0100

    Accessing nested elements

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -2,12 +2,12 @@ import { fromJS } from 'immutable';
 
 // This function returns the value of the object at the defined path
 export default function accessImmutableObject(object, array) {
-    // convert 'object' to immutable object
+  // convert 'object' to immutable object
   const immutableObject = fromJS(object);
 
-    // Used the `getIn` method to access the value at the defined path
+  // Used the `getIn` method to access the value at the defined path
   return immutableObject.getIn(array);
-};
+}
 
 // USAGE:
 // const result = accessImmutableObject({
@@ -15,6 +15,8 @@ export default function accessImmutableObject(object, array) {
 //          first: "Guillaume",
 //          last: "Salva"
 //     }
-// }, ['name', 'first']); // The second argument is the defined path the getIn method will traverse to get it's value. Returns 'undefined' if path doesn't exist.
+// }, ['name', 'first']);
+// The second argument is the defined path the getIn method will traverse to get 
+//it's value. Returns 'undefined' if path doesn't exist.
 
 // console.log(result);
