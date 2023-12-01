03c7f982772e9c19be32cc4e24dee3156248f3f8
MisturaO <misturaojulari91@gmail.com>
2023-12-01 12:56:52 +0100

    Converting into Immutable using Map

---
+++ 0x06-React_Immutable/1-map.js
@@ -0,0 +1,18 @@
+// import { Map } from 'immutable';
+import { Map } from  'immutable';
+
+
+const getImmutableObject = (object) => Map(object);
+
+export default getImmutableObject;
+
+// USAGE:
+// const object = {
+//     fear: true,
+//     smell: -1033575916.9145899,
+//     wall: false,
+//     thing: -914767132
+// }
+
+// const immutableObject = getImmutableObject(object);
+// console.log(immutableObject);
