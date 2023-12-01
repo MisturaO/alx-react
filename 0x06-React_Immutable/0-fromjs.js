a74d8f1e9dd1ea683e564675957ab12e2825c9b4
MisturaO <misturaojulari91@gmail.com>
2023-12-01 10:30:35 +0100

    Converting into an Immutable object using fromJS

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,16 +1,14 @@
 import { fromJS } from 'immutable';
 
-
-/*function getImmutableObject that accepts object as a parameter and converts 
+/* function getImmutableObject that accepts object as a parameter and converts
 it into an immutable Map using fromJS of the Immutable.js library */
-function getImmutableObject(object){
-    const immutableMap = fromJS(object);
+function getImmutableObject(object) {
+  const immutableMap = fromJS(object);
 
-    return immutableMap;
+  return immutableMap;
 }
 
-
-//USAGE:
+// USAGE:
 // const object = {
 //     fear: true,
 //     smell: -1033575916.9145899,
@@ -20,4 +18,4 @@ function getImmutableObject(object){
 
 // const immutableObject = getImmutableObject(object);
 // console.log(immutableObject);
-// console.log(immutableObject.toJS()); //converts immutable Map into to human readable format
+// console.log(immutableObject.toJS()); // converts immutable Map into to human readable format
