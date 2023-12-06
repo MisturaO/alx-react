967b83fdb2f9a845606f3640a0a923f51ac51dd9
MisturaO <misturaojulari91@gmail.com>
2023-12-06 12:30:35 +0100

    Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -16,22 +16,22 @@ export default function printBestStudents(obj) {
     lastName: x.lastName.charAt(0).toUpperCase() + x.lastName.slice(1)
     }));
 
-    return filtererdObj;
+    console.log(filtererdObj);
 }
 
-// const grades = {
-//     1: {
-//       score: 99,
-//       firstName: 'guillaume',
-//       lastName: 'salva',
-//     },
-//     2: {
-//         score: 60,
-//         firstName: 'Nana',
-//         lastName: 'alva',
-//       }
-//   };
+const grades = {
+    1: {
+      score: 99,
+      firstName: 'guillaume',
+      lastName: 'salva',
+    },
+    2: {
+        score: 60,
+        firstName: 'Nana',
+        lastName: 'alva',
+      }
+  };
 
-// // USAGE:
-// const result = printBestStudents(grades);
+// USAGE:
+printBestStudents(grades);
 // console.log(result.toJS());
